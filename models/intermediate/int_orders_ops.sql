484a3ef435dcdc1b2e835e162e5e3b3fe1d26b82
Lukas Dos Santos <ds.lukasgeii@gmail.com>
2026-02-05 11:28:27 +0000

    Created int_order_ops which gives the operational margin as an intermediate model

---
+++ models/intermediate/int_orders_ops.sql
@@ -0,0 +1,14 @@
+SELECT
+    om.orders_id,
+    om.date_date,
+    ROUND (om.margin + ship.shipping_fee - (ship.logcost + ship.ship_cost), 2) AS ops_margin,
+    om.quantity,
+    om.revenue,
+    om.margin,
+    ship.shipping_fee,
+    ship.ship_cost,
+    ship.logcost AS log_cost
+FROM ref{{("int_order_margin")}} AS om
+LEFT JOIN {{ref("stg_raw_data__ship")}} AS ship
+    USING (orders_id)
+ORDER BY orders_id DESC
